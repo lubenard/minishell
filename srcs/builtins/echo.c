8e4f09c62f9642eee87efda9a529aaf048f909f5
Luca BENARD <lubenard@student.42.fr>
2019-03-15 12:07:31 +0100

    Creation of very basic version of echo

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -1,27 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lubenard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/10 21:21:04 by lubenard          #+#    #+#             */
-/*   Updated: 2019/03/15 12:06:49 by lubenard         ###   ########.fr       */
+/*   Created: 2019/03/15 11:59:46 by lubenard          #+#    #+#             */
+/*   Updated: 2019/03/15 12:05:47 by lubenard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "../minishell.h"
 
-# include <unistd.h>
-# include "../libft/libft.h"
+int		echo(char **argv)
+{
+	i = 1;
 
-void	get_command(char *command, char **path);
-char	**get_path(char *path_line);
-
-/*
-** Builtins
-*/
-int		echo(char **argv);
-
-#endif
+	while (argv[i])
+		ft_putstr(argv[i]);
+	return (0);
+}
